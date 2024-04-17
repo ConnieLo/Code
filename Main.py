e90a579e90c8d6927645f435e0858c57765e8aa8
Conrad <conrad.lovegrove@gmail.com>
2024-04-17 09:30:42 +0100

    tfidf finish, added bert notebook

--- Main.py
+++ Main.py
@@ -1,10 +1,4 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('text.csv')
-
-text = []
-labels = []
-for row in df.iterrows():
-    text.append(row['text'])
-    labels.append(row['labels'])
+print('hello')
